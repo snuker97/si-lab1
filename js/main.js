d212aab11c366f1672923b93a6fba6e6205f0f4e
Eugeniu Racila <eugeniu.racila@gmail.com>
2018-02-15 02:20:38 +0200

    Third algorithm done

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const cesarBody = document.getElementById('cesar');
 cesarButton.addEventListener('click', () => {
     cesarBody.style.display = 'block';
     secondBody.style.display = 'none';
+    vernamBody.style.display = 'none';
 });
 
 const secondButton = document.getElementsByTagName('a')[1];
@@ -13,4 +14,15 @@ secondBody.style.display = 'none';
 secondButton.addEventListener('click', () => {
     secondBody.style.display = 'block';
     cesarBody.style.display = 'none';
+    vernamBody.style.display = 'none';
+});
+
+const vernamButton = document.getElementsByTagName('a')[2];
+const vernamBody = document.getElementById('vernam');
+vernamBody.style.display = 'none';
+
+vernamButton.addEventListener('click', () => {
+    vernamBody.style.display = 'block';
+    cesarBody.style.display = 'none';
+    secondBody.style.display = 'none';
 });
